9b1dd770abd278d45dadcb8b92cb3c21c87ce505
mims1234 <39203144+mims1234@users.noreply.github.com>
2018-05-27 20:17:52 +0530

    Add files via upload

--- commands/help.js
+++ commands/help.js
@@ -9,12 +9,12 @@ module.exports.run = async (bot,message,args) => {
     .setAuthor("Bot Help")
     .setColor('#ff34b3')
     .addField("Bot Commands",`
-**a$crates Bronze** -> *Opens Bronze Container*
+**$crates Bronze** -> *Opens Bronze Container*
 *__Note__ : In case you dont have any request from MiMs*
-\n**a$profile** -> *Displays your Profile*
-\n**a$profile paints <type>** -> *Displays your Paints*
+\n**$profile** ->*Displays your Profile*
+\n**$profile paints <type>** ->*Displays your Paints*
 *__Note__ : Types : common | uncommon | rare | epic | legendary | artefact*
-\n**a$profile preview <type> <paint-name>** -> *Displays the paint if owned*
+\n**$profile preview <type> <paint-name>** ->*Displays the paint if owned*
 *__Note__ : Types : common | uncommon | rare | epic | legendary | artefact*
 *__Note__ : Paints ex : Green | Space | Frost and etc*`);
 
