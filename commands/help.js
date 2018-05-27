616cbec97d7b879f72713a92a0895604b99b976d
mims1234 <39203144+mims1234@users.noreply.github.com>
2018-05-27 20:14:15 +0530

    Add files via upload

--- commands/help.js
+++ commands/help.js
@@ -13,7 +13,10 @@ module.exports.run = async (bot,message,args) => {
 *__Note__ : In case you dont have any request from MiMs*
 \n**a$profile** -> *Displays your Profile*
 \n**a$profile paints <type>** -> *Displays your Paints*
-*__Note__ : Types : common | uncommon | rare | epic | legendary | artefact*`);
+*__Note__ : Types : common | uncommon | rare | epic | legendary | artefact*
+\n**a$profile preview <type> <paint-name>** -> *Displays the paint if owned*
+*__Note__ : Types : common | uncommon | rare | epic | legendary | artefact*
+*__Note__ : Paints ex : Green | Space | Frost and etc*`);
 
     return message.channel.send(botembed);
 
