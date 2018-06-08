35d9cf82568e95e8d0739d1230682dff3a529191
mims1234 <39203144+mims1234@users.noreply.github.com>
2018-06-08 19:55:33 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -16,19 +16,7 @@ module.exports.run = async (bot,message,args) => {
 *__Note__ : Types : common | uncommon | rare | epic | legendary | artefact*
 \n**$profile preview <type> <paint-name>** ->*Displays the paint if owned*
 *__Note__ : Types : common | uncommon | rare | epic | legendary | artefact*
-*__Note__ : Paints ex : Green | Space | Frost and etc*`)
-    .addBlankField()
-    .addField('Premium Trial Commands [ BETA TESTING ]',`
-\n**$rainbow <type>**:
-*Changes role colors every 2 secs in a certain available patterns*\n
-**Instructions :**\n
-*#1 Create a role named ALMA make sure it has ADMIN Permission and default role color*\n
-*#2 Craate a role named rainbow make sure it has no Permissions at all and has defualt role color*\n
-*#3 Now make ALMA role highest Ranked Role in the Server and just below that should be rainbow role*\n
-*#4 Now just add the rainbow role to the member you want to have rainbow colored roles* :)\n
-*#5 Simple as that now you use the command with these custom types -> cop | rainbow | alien [NOTE: LowerCase Only]*\n
-**#NOTE You cannot change or stop this command untill 500 Secs**
-    `);
+*__Note__ : Paints ex : Green | Space | Frost and etc*`);
 
     return message.channel.send(botembed);
 
